6e506899d4f7df39166f346619fc1af6a99c9c33
Fred Ma <fred.ma@blackrock.com>
2020-07-15 15:38:24 -0400

    Added ConsumerDemoWithThreads and ConsumerRunnable

---
+++ src/main/scala/com/github/fma/kafka/tutorial1/ConsumerDemoWithThreads.scala
@@ -0,0 +1,29 @@
+package com.github.fma.kafka.tutorial1
+
+import java.util.concurrent.CountDownLatch
+
+import org.slf4j.LoggerFactory
+
+class ConsumerDemoWithThreads {}
+
+object ConsumerDemoWithThreads extends App {
+  val logger = LoggerFactory.getLogger(classOf[ConsumerDemoWithThreads])
+
+  val bootstrapServers = "127.0.0.1:9092"
+  val groupId = "my-sixth-application"
+  val topic = "first_topic"
+
+  val latch = new CountDownLatch(1)
+  val myConsumerRunnable = new ConsumerRunnable(bootstrapServers, groupId, topic, latch)
+  val myThread = new Thread(myConsumerRunnable)
+
+  scala.sys.addShutdownHook({
+    logger.info("Caught shutdown hook")
+    myConsumerRunnable.shutdown()
+    latch.await()
+  })
+
+  myThread.start()
+  latch.await()
+  logger.info("Application is closing")
+}
---
+++ src/main/scala/com/github/fma/kafka/tutorial1/ConsumerRunnable.scala
@@ -0,0 +1,49 @@
+package com.github.fma.kafka.tutorial1
+
+import java.util.Properties
+import java.util.concurrent.CountDownLatch
+
+import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.slf4j.{Logger, LoggerFactory}
+
+import scala.concurrent.duration._
+import scala.jdk.CollectionConverters._
+import scala.jdk.DurationConverters._
+import scala.language.postfixOps
+import scala.util.Try
+
+class ConsumerRunnable(bootstrapServers: String, groupId: String, topic: String, latch: CountDownLatch) extends Runnable {
+  val logger: Logger = LoggerFactory.getLogger(classOf[ConsumerRunnable])
+
+  val properties = new Properties();
+  properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
+  properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getName)
+  properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getName)
+  properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId)
+  properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+  val consumer = new KafkaConsumer[String, String](properties)
+  consumer.subscribe(List(topic).asJava)
+
+  val pollFunc: () => Unit = () => {
+    while(true) {
+      consumer.poll((100 milliseconds).toJava).asScala.foreach(record => {
+        logger.info(s"Key: ${record.key()}, Value: ${record.value()}")
+        logger.info(s"Partition: ${record.partition()}, Offset: ${record.offset()}")
+      })
+    }
+  }
+
+  override def run(): Unit = {
+    Try(pollFunc).recover(_ => {
+      logger.info("Received shutdown signal!")
+      latch.countDown()
+      consumer.close()
+    })
+  }
+
+  def shutdown(): Unit = {
+    // Interrupts consumer.poll()
+    consumer.wakeup()
+  }
+}
