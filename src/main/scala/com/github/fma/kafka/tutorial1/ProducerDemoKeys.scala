c0b946398920841918da77d2741b066ccd1f9877
Fred Ma <fred.ma@blackrock.com>
2020-07-15 14:52:12 -0400

    Added ProducerDemoKeys

---
+++ src/main/scala/com/github/fma/kafka/tutorial1/ProducerDemoKeys.scala
@@ -0,0 +1,61 @@
+package com.github.fma.kafka.tutorial1
+
+import java.util.Properties
+
+import org.apache.kafka.clients.producer._
+import org.apache.kafka.common.serialization.StringSerializer
+import org.slf4j.LoggerFactory
+
+class ProducerDemoKeys {}
+
+object ProducerDemoKeys extends App {
+  val logger = LoggerFactory.getLogger(classOf[ProducerDemoKeys])
+
+  val bootstrapServers = "127.0.0.1:9092"
+  val callback = new Callback {
+    override def onCompletion(recordMetadata: RecordMetadata, e: Exception): Unit = {
+      Option(e) match {
+        case Some(_) =>
+          logger.error("Error while producing", e)
+        case None =>
+          logger.info(s"""Received new metadata:
+                         |Topic: ${recordMetadata.topic()}
+                         |Partition: ${recordMetadata.partition()}
+                         |Offset: ${recordMetadata.offset()}
+                         |Timestamp: ${recordMetadata.timestamp()}""")
+      }
+    }
+  }
+
+  // create Producer properties
+  val properties = new Properties();
+  properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
+  properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
+  properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
+
+  // create a producer
+  val producer = new KafkaProducer[String, String](properties);
+
+  (0 to 9).foreach(idx => {
+    val topic = "first_topic"
+    val value = s"hello_world $idx"
+    val key = s"id_$idx"
+
+    logger.info(s"Key: $key")
+    // id_0 -> partition 1
+    // id_1 -> partition 0
+    // id_2 -> partition 2
+
+    // create a producer record
+    val record = new ProducerRecord[String, String](topic, key, value)
+
+    // send data
+    producer.send(record, callback).get() // block send to make synchronous - don't do this in production!
+  })
+
+  // flush data
+  producer.flush()
+
+  // flush and close producer
+  producer.close()
+}
